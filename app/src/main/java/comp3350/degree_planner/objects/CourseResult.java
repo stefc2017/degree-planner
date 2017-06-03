7ca738a09181f4013a1c20c49a3d5920873c3d50
Stefan <stefc2013@gmail.com>
2017-06-02 21:30:53 -0500

    Issue-8: See courses not yet taken
    Make methods to get  the courses that the student has not yet taken. I also added a few getter methods in CourseResult and added methods to get all courses, get courses the student has taken and the courses the student has not yet taken.
    Spent: 3 hours

---
+++ app/src/main/java/comp3350/degree_planner/objects/CourseResult.java
@@ -0,0 +1,32 @@
+package comp3350.degree_planner.objects;
+
+/**
+ * Created by Kaleigh on 2017-05-31.
+ *
+ * A CourseResult is a simple object that holds a grade for a course completed as
+ * well as the student who completed it, as IDs.
+ */
+
+public class CourseResult {
+    private int id;         // Id of the object, since a student may take a course more than once
+    private int courseId;
+    private int studentId;
+    private int gradeId;
+
+    public CourseResult(int id, int courseId, int studentId, int gradeId) {
+        this.id = id;
+        this.courseId = courseId;
+        this.studentId = studentId;
+        this.gradeId = gradeId;
+    }
+
+    public int getId() { return id; }
+
+    public int getStudentId(){
+        return studentId;
+    }//end getStudentId
+
+    public int getCourseId(){
+        return courseId;
+    }//end getCourseId
+}
