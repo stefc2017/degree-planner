24851e87520a5ccd908a99c939aba5ce209c5751
He Penny <arupakahime@gmail.com>
2017-06-04 16:48:36 -0500

    add business layer

---
+++ app/src/main/java/comp3350/degree_planner/business/AccessCourses.java
@@ -0,0 +1,23 @@
+package comp3350.degree_planner.business;
+
+import java.util.List;
+
+import comp3350.degree_planner.application.Main;
+import comp3350.degree_planner.application.Services;
+import comp3350.degree_planner.objects.Course;
+import comp3350.degree_planner.persistence.DataAccessStub;
+
+public class AccessCourses
+{
+	private DataAccessStub dataAccess;
+
+	public AccessCourses()
+	{
+		dataAccess = (DataAccessStub) Services.getDataAccess();
+	}
+
+    public String getCourses(List<Course> courses)
+    {
+        return null;
+    }
+}
