7e48ac0121cfe921710ed88807e64471be7b9f81
He Penny <arupakahime@gmail.com>
2017-06-05 04:34:12 -0500

    create basic UI for viewing degree requirements

--- app/src/main/java/comp3350/degree_planner/presentation/MainActivity.java
+++ app/src/main/java/comp3350/degree_planner/presentation/MainActivity.java
@@ -1,15 +1,31 @@
 package comp3350.degree_planner.presentation;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import comp3350.degree_planner.application.Main;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import comp3350.degree_planner.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Main.startUp();
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Main.shutDown();
+    }
+
+    public void buttonDegreesOnClick(View v){
+        Intent intent = new Intent(MainActivity.this, DegreesActivity.class);
+        MainActivity.this.startActivity(intent);
     }
 }
