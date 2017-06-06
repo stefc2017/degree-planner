1665bd198c0369b88f2fb7d18e0fc28037a89426
Farhad Davudov <farhad.davudov@gmail.com>
2017-06-06 04:05:04 -0500

    Revert "Revert "Issue 9: Create lists of historical course offerings by semester""
    This reverts commit a09d15243327da95ae7f82e3b21f84a5596fe5c8.

--- app/src/main/java/comp3350/degree_planner/business/AccessCourses.java
+++ app/src/main/java/comp3350/degree_planner/business/AccessCourses.java
@@ -1,14 +1,11 @@
 package comp3350.degree_planner.business;
-
 import java.util.List;
-
-import comp3350.degree_planner.application.Main;
 import comp3350.degree_planner.application.Services;
 import comp3350.degree_planner.objects.Course;
+import comp3350.degree_planner.objects.ScienceCourse;
 import comp3350.degree_planner.persistence.DataAccessStub;
 
-public class AccessCourses
-{
+public class AccessCourses {
 	private DataAccessStub dataAccess;
 
 	public AccessCourses()
@@ -16,8 +13,23 @@ public class AccessCourses
 		dataAccess = (DataAccessStub) Services.getDataAccess();
 	}
 
-    public String getCourses()
+    public String getCourses(int degreeId, List<Course> courses)
     {
-        return null;
+		String result = null;
+		final int COMP_SCI_ID = 1;
+
+        if(courses != null && degreeId == COMP_SCI_ID) {
+			courses.clear();
+			courses.add(new ScienceCourse(1, "Introductory Computer Science I", 3.0, 1,
+					1010, "Basic programming concepts."));
+			courses.add(new ScienceCourse(2, "Introductory Computer Science II", 3.0,
+					1, 1020, "More basic programming concepts."));
+			courses.add(new ScienceCourse(3, "Object Orientation", 3.0, 1,
+					2150, "Detailed look at proper object oriented programming."));
+            for(int i = 0; i < 25; i++){
+                courses.add(new ScienceCourse(3+i, "Course" + i, 3.0, 1, 1020+i, "Description"));
+            }
+		}
+		return result;
     }
 }
