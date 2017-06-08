b95f0302787f7a11f525442b20661616ea2b1075
He Penny <arupakahime@gmail.com>
2017-06-07 23:27:10 -0500

    add new test suites

--- app/src/test/java/comp3350/degree_planner/tests/AllTests.java
+++ app/src/test/java/comp3350/degree_planner/tests/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp3350.degree_planner.tests.business.GetCompletedCoursesTest;
+<<<<<<< refs/remotes/origin/master
 import comp3350.degree_planner.tests.objects.CourseOfferingTest;
 import comp3350.degree_planner.tests.objects.CoursePlanTest;
 import comp3350.degree_planner.tests.objects.CoursePrerequisiteTest;
@@ -19,10 +20,10 @@ import comp3350.degree_planner.tests.objects.ScienceCourseTest;
 import comp3350.degree_planner.tests.objects.StudentTest;
 import comp3350.degree_planner.tests.objects.TermTypeTest;
 import comp3350.degree_planner.tests.objects.UserDefinedCourseTest;
-=======
+
 import comp3350.degree_planner.tests.business.GetDegreeCoursesTest;
 import comp3350.degree_planner.tests.business.GetDegreesTest;
->>>>>>> add new test suites
+
 
 /**
  * Created by Tiffany Jiang on 2017-06-04.
@@ -53,7 +54,7 @@ import comp3350.degree_planner.tests.business.GetDegreesTest;
         UserDefinedCourseTest.class
 })
 
-<<<<<<< refs/remotes/origin/master
+
 public class AllTests {
 =======
     private static void testBusiness() {
@@ -61,5 +62,5 @@ public class AllTests {
         suite.addTestSuite(GetDegreeCoursesTest.class);
         suite.addTestSuite(GetDegreesTest.class);
     }
->>>>>>> add new test suites
+
 }
---
+++ app/src/test/java/comp3350/degree_planner/tests/business/GetCourseOfferingsByTermTest.java
@@ -0,0 +1,21 @@
+package comp3350.degree_planner.tests.business;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+import comp3350.degree_planner.business.AccessDegrees;
+import comp3350.degree_planner.objects.Degree;
+
+import comp3350.degree_planner.persistence.DataAccess;
+import comp3350.degree_planner.persistence.DataAccessStub;
+
+/**
+ * Created by F.D. on 07/06/17.
+ */
+
+public class GetCourseOfferingsByTermTest {
+    
+}
