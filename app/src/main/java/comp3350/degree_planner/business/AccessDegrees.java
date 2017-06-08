db612668399a20c12c0b1f417a2b0dea587eb1a8
He Penny <arupakahime@gmail.com>
2017-06-07 23:55:24 -0500

    updates UI and business layer to use DataAccess Interface

--- app/src/main/java/comp3350/degree_planner/business/AccessDegrees.java
+++ app/src/main/java/comp3350/degree_planner/business/AccessDegrees.java
@@ -1,23 +1,19 @@
 package comp3350.degree_planner.business;
 
-import java.util.List;
 import java.util.ArrayList;
 
-import comp3350.degree_planner.application.Services;
 import comp3350.degree_planner.objects.Degree;
-import comp3350.degree_planner.persistence.DataAccessStub;
+import comp3350.degree_planner.persistence.DataAccess;
 
 /**
  * Created by Penny He on 6/4/2017.
  */
 
 public class AccessDegrees {
-    private DataAccessStub dataAccess;
+    private DataAccess dataAccess;
 
-    public AccessDegrees()
-    {
-        dataAccess = (DataAccessStub) Services.getDataAccess();
-    }
+    public AccessDegrees(DataAccess dataAccess){ this.dataAccess = dataAccess; }
+
 
     public ArrayList<Degree> getAllDegrees()
     {
