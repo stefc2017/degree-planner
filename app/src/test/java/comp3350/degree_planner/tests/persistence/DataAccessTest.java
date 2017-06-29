bf4a0d188965b977f3d60ca0aa65e4fbefd3755d
provencm <matt94@gmail.com>
2017-06-28 22:19:44 -0500

    testGetCourse complete

--- app/src/test/java/comp3350/degree_planner/tests/persistence/DataAccessTest.java
+++ app/src/test/java/comp3350/degree_planner/tests/persistence/DataAccessTest.java
@@ -4,17 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.degree_planner.application.Main;
 import comp3350.degree_planner.application.Services;
 import comp3350.degree_planner.objects.Course;
+import comp3350.degree_planner.objects.CourseResult;
+import comp3350.degree_planner.objects.GradeType;
+import comp3350.degree_planner.objects.ScienceCourse;
+import comp3350.degree_planner.objects.Student;
 import comp3350.degree_planner.persistence.DataAccess;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by pennyhe on 2017-06-28.
+ * Modified by Matthew Provencher on 2017-06-28
  */
 
 public class DataAccessTest {
@@ -31,7 +39,28 @@ public class DataAccessTest {
     }
 
     @Test
-    public void testgetCourse() {}
+    public void testgetCourse() {
+        List<Course> courses = dataAccess.getAllCourses();
+        List<Course> noCourses = new ArrayList<Course>();
+        CourseResult courseResult = new CourseResult(1, new ScienceCourse(1, "Introductory Computer Science I",
+                3.0, 1, 1010, "Basic programming concepts."), new Student(1, 1234567, "Jim Bob",
+                "jimbob@myumanitoba.ca", "helloworld1", 1), new GradeType(1, "A+", 4.5));
+        Course result;
+
+        System.out.println("Beginning getCourse tests");
+
+        // Valid courses test
+        result = dataAccess.getCourse( courseResult, courses );
+        assertNotNull( result );;
+        assertEquals( "Introductory Computer Science I", result.getName() );
+        result = null;
+
+        // Invalid courses test
+        result = dataAccess.getCourse( courseResult, noCourses );
+        assertNull( result );
+
+        System.out.println("getCourse tests complete");
+    }
 
     @Test
     public void testGetAllCourses() {
