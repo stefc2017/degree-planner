260a04f67e7dad8c3d0a13ab0107af7a82953534
He Penny <arupakahime@gmail.com>
2017-07-09 14:27:51 -0500

    add view electives page and route it to add page

--- app/src/main/java/comp3350/degree_planner/business/AccessCourses.java
+++ app/src/main/java/comp3350/degree_planner/business/AccessCourses.java
@@ -1,18 +1,39 @@
 package comp3350.degree_planner.business;
+import java.util.ArrayList;
 import java.util.List;
 
 import comp3350.degree_planner.objects.Course;
 import comp3350.degree_planner.objects.CourseOffering;
 import comp3350.degree_planner.objects.TermType;
 
+import comp3350.degree_planner.objects.UserDefinedCourse;
 import comp3350.degree_planner.persistence.DataAccess;
 
 public class AccessCourses {
 	private DataAccess dataAccess;
-
+	private List<Course> courses = new ArrayList<>();
+	int id = 6;
 	public AccessCourses(DataAccess dataAccess) { this.dataAccess = dataAccess; }
 
     public List<Course> getDegreeCourses(int degreeId) { return dataAccess.getDegreeCourses(degreeId); }
+
+	public List<Course> getAllUserDefinedCourses() {
+		courses.add(new UserDefinedCourse(5, "Cultural Anthropology", 3.0, "ANTH 1220"));
+		courses.add(new UserDefinedCourse(6, "Language and Culture", 3.0, "ANTH 2370"));
+		return courses;
+	}
+
+	public void removeUserDefinedCourse(int courseId) {
+		for(Course c : courses){
+			if(c.getId() == courseId){
+				courses.remove(c);
+			}
+		}
+	}
+
+	public void addUserDefinedCourse(String name, double creditHours, String abbreviation){
+		courses.add(new UserDefinedCourse(++id, name, creditHours, abbreviation));
+	}
 
 	public Course getCourseById(int courseId) { return dataAccess.getCourseById(courseId); }
 
--- app/src/main/java/comp3350/degree_planner/presentation/AddFreeElectiveActivity.java
+++ app/src/main/java/comp3350/degree_planner/presentation/AddFreeElectiveActivity.java
@@ -5,15 +5,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
 import comp3350.degree_planner.R;
+import comp3350.degree_planner.application.Services;
+import comp3350.degree_planner.business.AccessCourses;
+import comp3350.degree_planner.objects.Course;
 
 /**
  * Created by Penny He on 7/9/2017.
  */
 
 public class AddFreeElectiveActivity extends AppCompatActivity {
+    private AccessCourses accessCourses;
+    private List<Course> freeElectives = null;
+    private ArrayAdapter<Course> electiveListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +40,4 @@ public class AddFreeElectiveActivity extends AppCompatActivity {
         pageTitle.setText("Add Free Elective To Course Plan");
     }
 
-    public void buttonAddNewElectiveOnClick(View v){
-        Intent intent = new Intent(AddFreeElectiveActivity.this, CreateElectiveActivity.class);
-        AddFreeElectiveActivity.this.startActivity(intent);
-    }
 }
