32b90b0a23ff7e7dd9b0fd8292279c348ecd7b4f
Stefan <stefc2013@gmail.com>
2017-07-11 09:15:43 -0500

    Issue-119 - Acceptance Test for Pick Degree Wrote the acceptance test for pick degree. There is 2 methods, 1 to test for picking the computer science major degree and the second for picking the computer science honours degree Spent: 1 hour

---
+++ app/src/androidTest/java/comp3350/degree_planner/acceptance/PickDegreeTest.java
@@ -0,0 +1,81 @@
+package comp3350.degree_planner.acceptance;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.degree_planner.R;
+import comp3350.degree_planner.presentation.CoursePlansActivity;
+import comp3350.degree_planner.presentation.DegreeProgressActivity;
+import comp3350.degree_planner.presentation.MainActivity;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class PickDegreeTest {
+    private static final String PICK_DEGREE_TITLE = "Pick a Degree";
+    private static final String DEGREE_1 = "Computer Science Major";
+    private static final String DEGREE_2 = "Computer Science Honours";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    private Solo solo;
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),
+                activityTestRule.getActivity());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+    @Test
+    public void testPickCSMajorDegree() throws Exception {
+
+        //Navigate to Course Planning
+        solo.unlockScreen();
+        solo.waitForActivity("MainActivity");
+        solo.clickOnButton(solo.getString(R.string.progress));
+        solo.assertCurrentActivity("Expected Degree Progress Activity", DegreeProgressActivity.class);
+
+        solo.clickOnButton(solo.getString(R.string.pick_degree));
+        assertTrue ("Pick Degree screen has pop up", solo.searchText(PICK_DEGREE_TITLE));
+        assertTrue (DEGREE_1 + " degree was found", solo.searchText(DEGREE_1));
+        assertTrue (DEGREE_2 + " degree was found", solo.searchText(DEGREE_2));
+
+        solo.clickOnText(DEGREE_1);
+        solo.assertCurrentActivity("Expected Degree Progress Activity", DegreeProgressActivity.class);
+    }
+
+    @Test
+    public void testPickCSHonourDegree() throws Exception {
+
+        //Navigate to Course Planning
+        solo.unlockScreen();
+        solo.waitForActivity("MainActivity");
+        solo.clickOnButton(solo.getString(R.string.progress));
+        solo.assertCurrentActivity("Expected Degree Progress Activity", DegreeProgressActivity.class);
+
+        solo.clickOnButton(solo.getString(R.string.pick_degree));
+        assertTrue ("Pick Degree screen has pop up", solo.searchText(PICK_DEGREE_TITLE));
+        assertTrue (DEGREE_1 + " degree was found", solo.searchText(DEGREE_1));
+        assertTrue (DEGREE_2 + " degree was found", solo.searchText(DEGREE_2));
+
+        solo.clickOnText(DEGREE_2);
+        solo.assertCurrentActivity("Expected Degree Progress Activity", DegreeProgressActivity.class);
+    }
+}
